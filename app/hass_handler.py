cca5e8799574b6c223b7ef909cada101c61d39eb
Jonah Kraushaar <jonah.kraushaar@gmx.net>
2020-07-15 22:57:18 +0200

    Generalized service call and added further logic

--- app/hass_handler.py
+++ app/hass_handler.py
@@ -1,5 +1,7 @@
 from hass_websocket_client import HassWsClient
 
+from rhasspyhermes.nlu import NluIntent
+from typing import Optional
 import asyncio
 
 loop = asyncio.get_event_loop()
@@ -8,25 +10,48 @@ URL = "ws://localhost:8123/api/websocket"
 TOKEN = "exampletoken123456789"
 STANDARDSERVICES = ["turn_on","turn_off","toggle","update_entity"]
 
+def extract_slot_value(slots: list, slot_name: str, default=None):
+    slot = next(filter(lambda slot: slot.slot_name == slot_name, slots), None)
+    if slot and slot.value["kind"] is not "Unknown":
+        return slot.value.get("value", default)
+    return default
+
 class HassHandler:
     def __init__(self):
         self.ws = HassWsClient(URL,token = TOKEN)
+        #{'serviceX':{'domains'=['domain1','domain2'], 'required_fields':['slot1', 'slot2']}}
+        self.local_services = {}
         #Task management is going to happen here in the future
-        loop.run_until_complete(self.update_external_data())
+        loop.run_until_complete(self.update_local_data())
+
+    async def handle_service_intent(self, intent):
+        # one/two of entity, room, user must be passed to
+        slots: dict = intent.slots
+        entity_flag, entity_id, domain = self.inspect_entity(slots.get('entity',None),room=slots.get('room',None),user=slots.get('user',None))
+        #TODO find out where to find intent name
+        service = intent.intentName
+        try:
+            service_flag, nescessary_fields = self._approve_service(service, domain)
+        except:
+            service_flag = False
+        
+        if entity_flag and service_flag:
+            service_data = {}
+            for field in nescessary_fields:
+                try:
+                    service_data[str(field)] = slots[str(field)]
+                except:
+                    #TODO specify this for multiple missing fields
+                    return "Some essential Information is missing."
+            tracer = await self.ws.call_service(domain, service, service_data = service_data)
+            if tracer:
+                pass#All Successfull
+            else:
+                return "Could you rephrase your request?"
 
     
-    def handle_service_call(self, entity: str, service:str, **kwargs):
-        tracer, entity_id, domain = self.inspect_entity(entity, room = None, user = None)
-        if tracer:
-            #TODO replace with approving function
-            if service in STANDARDSERVICES:
-                domain = "homeassistant"
-            loop.run_until_complete(self.ws.call_service(domain,service,service_data={entity_id:entity}))
-        else:
-            return "No valid Entity Id got passed. Sorry!"
-    
     def inspect_entity(self, entity:str, room:str = "$ALL", user:str = "$ALL")->(bool,str,str):
-        """Checks if the passed string is a valid entity_id and tries to convert it.
+        """Checks if the passed string is a valid entity_id otherwise tries to convert it.
         Parameters
         ----------
         entity : str
@@ -37,6 +62,8 @@ class HassHandler:
             if the returned entity_id is valid
         str
             the valid entity_id
+        str
+            the domain of the entity_id
         Raises
         ------
         """
@@ -56,7 +83,29 @@ class HassHandler:
         #Option 2: Synonym checking
         pass
 
-    async def update_external_data(self):
+    def _approve_service(self, service: str, domain: str)->(bool, list):
+        try:
+            service_item = self.local_services[service]
+            if domain in service_item['domains']:
+                #FIXME This can throw an exception
+                required_fields = service_item['req_fields']
+                return (True, required_fields)
+        except:
+            pass
+        return (False, None)
+    
+    async def __format_fetch_services(self):
+        formatted_services = {}
+        flag, services = self.ws.fetch_services()
+        if flag:
+            for domain in services:
+                for service in services[domain]:
+                    if not formatted_services[service]['domains']:
+                        formatted_services[service]['domains'] = []
+                    formatted_services[service]['domains'].append(domain)
+                    #TODO write req field per domain
+
+    async def update_local_data(self):
         tracer, domains = await self.ws.fetch_domains()
         self.domains = domains if tracer else []
         tracer, entity_ids = await self.ws.fetch_entity_ids()
--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
+from hass_handler import HassHandler
+
 import logging
 
-from hass_handler import HassHandler
+import asyncio
 
 from rhasspyhermes.nlu import NluIntent
 from rhasspyhermes_app import EndSession, HermesApp
@@ -13,16 +15,15 @@ logger_format = logging.Formatter('%(filename)s,%(lineno)s - %(levelname)s - %(m
 log_stream.setFormatter(logger_format)
 logger.addHandler(log_stream)
 
-
+loop = asyncio.get_event_loop()
 app = HermesApp("HassApp")
 
 hass = HassHandler()
 
 def extract_slot_value(intent: NluIntent, slot_name: str, default=None):
     slot = next(filter(lambda slot: slot.slot_name == slot_name, intent.slots), None)
-    if slot:
-        if slot.value["kind"] != "Unknown":
-            return slot.value.get("value", default)
+    if slot and slot.value["kind"] is not "Unknown":
+        return slot.value.get("value", default)
     return default
 
 @app.on_intent("hassapp.TurnOn")
@@ -31,7 +32,13 @@ def hass_TurnOn(intent: NluIntent):
     #room = extract_slot_value(intent, "room")
     #user = extract_slot_value(intent, "user")
     if entity:
-        text = hass.handle_service_call(str(entity), "") or None
+        text = hass.handle_service_intent(intent) or None
     else:
         text = "Passing no Entity is not yet supported"
     return EndSession(text if not entity else None)
+
+"""
+@app.on_prefix("hassapp.")
+def hass_Intent(intent: NluIntent):
+    return EndSession(hass.handle_intent(intent))
+"""
